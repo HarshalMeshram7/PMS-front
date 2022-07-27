f3d2b4d1ab092bfd47e8a1d9049d460c0dcadb15
monish <monish.barse@upcred.in>
2022-07-27 18:06:34 +0530

    server change

--- src/services/apiConfig.js
+++ src/services/apiConfig.js
@@ -1,2 +1,2 @@
-// export const MAIN_URL = "https://expressjwttoken.herokuapp.com";
-export const MAIN_URL = "http://localhost:8000";
+export const MAIN_URL = "https://expressjwttoken.herokuapp.com";
+// export const MAIN_URL = "http://localhost:8000";

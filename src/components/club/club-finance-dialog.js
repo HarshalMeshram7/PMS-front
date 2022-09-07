793718f9856ea8d4d959f7a39c90a6aa2c73b8bf
monish <monishbarse9@gmail.com>
2022-09-07 16:28:40 +0530

    finance update

--- src/components/club/club-finance-dialog.js
+++ src/components/club/club-finance-dialog.js
@@ -1,24 +1,17 @@
-import * as React from 'react';
-import {
-    Button,
-    Dialog,
-    DialogActions,
-    DialogContent,
-    Typography,
-    Grid
-} from "@mui/material";
+import * as React from "react";
+import { Button, Dialog, DialogActions, DialogContent, Typography, Grid, Container } from "@mui/material";
 import { useState } from "react";
 import LoadingBox from "src/components/common/loading-box";
 import { useSnackbar } from "notistack";
 
-import Box from '@mui/material/Box';
-import Tabs from '@mui/material/Tabs';
-import Tab from '@mui/material/Tab';
-
+import Box from "@mui/material/Box";
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
+import ClubFinance from "./finance-component/club-finance";
 
 export const ClubFinanceDialog = ({ open, handleClose, club, mutate }) => {
     const { enqueueSnackbar } = useSnackbar();
-    
+
     const [loading, setLoading] = useState();
 
     const [value, setValue] = React.useState(0);
@@ -27,18 +20,17 @@ export const ClubFinanceDialog = ({ open, handleClose, club, mutate }) => {
         setValue(newValue);
     };
 
-
     function LinkTab(props) {
         return (
-          <Tab
-            component="a"
-            onClick={(event) => {
-              event.preventDefault();
-            }}
-            {...props}
-          />
+            <Tab
+                component="a"
+                onClick={(event) => {
+                    event.preventDefault();
+                }}
+                {...props}
+            />
         );
-      }
+    }
 
     return (
         <Dialog
@@ -50,45 +42,41 @@ export const ClubFinanceDialog = ({ open, handleClose, club, mutate }) => {
                 style: { backgroundColor: "#121212dd" },
             }}
         >
-
             {loading && <LoadingBox />}
-          
-            <DialogContent>
-                <Typography 
-                align='center'
-                variant="h4"
-                
-                >Club Finance</Typography>
-            </DialogContent>
 
+            <DialogContent style={{height:'600px'}}>
+                <Typography align="center" variant="h4">
+                    Club Finance
+                </Typography>
+            
             <Grid>
-                <Box sx={{ width: '100%', bgcolor: 'background.paper' }}>
+                <Box sx={{ width: "100%", bgcolor: "background.paper" }}>
                     <Tabs value={value} onChange={handleChange} centered>
-                        <LinkTab value= "0" label="Club Finance" />
-                        <LinkTab value= "1" label="Ecommerse Site" />
-                        <LinkTab value= "2" label="Club Earning" />
-                        <LinkTab value= "3" label="Club Expenses" />
-                        <LinkTab value= "4" label="Club Budget" />
-                        <LinkTab value= "5" label="Club Event" />
-                        <LinkTab value= "6" label="Club Organization" />
-                        <LinkTab value= "7" label="Club Statistic" />
+                        <LinkTab value="0" label="Club Finance" />
+                        <LinkTab value="1" label="Ecommerse Site" />
+                        <LinkTab value="2" label="Club Earning" />
+                        <LinkTab value="3" label="Club Expenses" />
+                        <LinkTab value="4" label="Club Budget" />
+                        <LinkTab value="5" label="Club Event" />
+                        <LinkTab value="6" label="Club Organization" />
+                        <LinkTab value="7" label="Club Statistic" />
                     </Tabs>
-                    {value == 0 && <>Club Finance</>}
-                    {value == 1 && <>Ecommerse</>}
-                    {value == 2 && <>Club Earning</>}
-                    {value == 3 && <>Club Expenses</>}
-                    {value == 4 && <>Club Budget</>}
-                    {value == 5 && <>Club Event</>}
-                    {value == 6 && <>Club Organization</>}
-                    {value == 7 && <>Club Statistic</>}
                 </Box>
             </Grid>
-
+            <Container maxWidth="md">
+                        {value == "0" && <ClubFinance/>}
+                        {value == "1" && <>Ecommerse</>}
+                        {value == "2" && <>Club Earning</>}
+                        {value == "3" && <>Club Expenses</>}
+                        {value == "4" && <>Club Budget</>}
+                        {value == "5" && <>Club Event</>}
+                        {value == "6" && <>Club Organization</>}
+                        {value == "7" && <>Club Statistic</>}
+            </Container>
+            </DialogContent>
             <DialogActions>
                 <Button onClick={handleClose}>Close</Button>
             </DialogActions>
-            
         </Dialog>
-
     );
 };

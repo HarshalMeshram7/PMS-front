0f11306086b0e9e788635d8d2658b1719810b7d4
Wasif <swasifr@gmail.com>
2022-10-03 13:21:28 +0530

    academy finance working update

--- src/components/academy/academyfinance-component/academy-budget.js
+++ src/components/academy/academyfinance-component/academy-budget.js
@@ -10,26 +10,38 @@ import {
   Typography,
   FormGroup,
   FormControlLabel,
+  FormLabel,
   Switch,
+  FormControl,
   TextareaAutosize
 } from "@mui/material";
+import { bool } from "prop-types";
 
 export default function AcademyBudget() {
   const formik = useFormik({
     initialValues: {
-      ApprovedbyMembersofTheCommittee: "",
       BudgetDetails: "",
+
     },
     validationSchema: Yup.object({}),
 
     onSubmit: async (data) => {
       try {
-        console.log(data);
+        let finalData = { ...data, approvedmember: approvedmember }
+        console.log(finalData);
       } catch (error) {
         console.log(error);
       }
     },
   });
+
+
+  const [approvedmember, setApprovedmember] = React.useState(false);
+
+  const handleChange = (event) => {
+    setApprovedmember(event.target.checked,);
+  };
+
 
   return (
     <>
@@ -42,10 +54,21 @@ export default function AcademyBudget() {
               </Grid>
 
               <Grid item md={6} xs={12}>
-                <FormGroup>
-                  <FormControlLabel control={<Switch defaultChecked />} label="" />
-                </FormGroup>
+                <FormControl component="fieldset" variant="standard">
+                  {/* <FormLabel component="legend">Assign responsibility</FormLabel> */}
+                  <FormGroup>
+                    <FormControlLabel
+                      control={
+                        <Switch checked={approvedmember} onChange={handleChange} name="approvedmember" />
+                      }
+                      label="Are you a Approved Member?"
+                    />
+
+                  </FormGroup>
+                </FormControl>
               </Grid>
+
+
 
               <Grid item md={6} xs={12}>
                 <Typography>Budget Details : </Typography>
@@ -55,11 +78,22 @@ export default function AcademyBudget() {
                 <TextareaAutosize
                   minRows={6}
                   style={{ width: 200 }}
+                  error={Boolean(formik.touched.BudgetDetails && formik.errors.BudgetDetails)}
+                  fullWidth
+                  helperText={formik.touched.BudgetDetails && formik.errors.BudgetDetails}
+                  label="Budget Details"
+                  margin="dense"
+                  name="BudgetDetails"
+                  onBlur={formik.handleBlur}
+                  onChange={formik.handleChange}
+                  type="number"
+                  value={formik.values.BudgetDetails}
+                  variant="outlined"
                 />
               </Grid>
 
               <Grid item md={12} xs={12} textAlign="center">
-                <Button variant="outlined" color="primary">
+                <Button type="submit" variant="outlined" color="primary">
                   Save
                 </Button>
               </Grid>
